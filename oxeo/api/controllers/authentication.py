4436d79225784c049091238b52c2b5ee1473db5d
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-06-01 17:44:58 +0000

    add auth controller

---
+++ oxeo/api/controllers/authentication.py
@@ -0,0 +1,108 @@
+import os
+from datetime import datetime, timedelta
+from typing import Union
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+
+from oxeo.api.models import database, schemas
+
+SECRET_KEY = os.environ.get("SERVER-SECRET", "my-secret")
+ALGORITHM = os.environ.get("SERVER-ALGORITHM", "HS256")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, email: str):
+    return db.query(database.User).filter(database.User.email == email).first()
+
+
+def authenticate_user(db, email: str, password: str):
+    user = get_user(db, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+d_token = Depends(oauth2_scheme)
+d_session = Depends(database.get_db)
+
+
+async def get_current_user(token: str = d_token, db: Session = d_session):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+d_user = Depends(get_current_user)
+
+
+async def get_current_active_user(
+    current_user: schemas.User = d_user,
+):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    print("CREAT_USER")
+    print(hashed_password)
+    db_user = database.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(database.Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user: int):
+    db_item = database.Item(**item.dict(), owner_id=user.id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
