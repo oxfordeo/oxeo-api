7c76db0439a36a372e2507a9e0563f01f89c8f0d
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-06-01 17:53:27 +0000

    rm flake8 bubbear b008

---
+++ oxeo/api/routes.py
@@ -0,0 +1,61 @@
+from datetime import timedelta
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from oxeo.api.controllers import authentication as auth
+from oxeo.api.models import database, schemas
+
+router = APIRouter()
+
+requires_auth = [Depends(auth.get_current_active_user)]
+
+
+@router.post("/auth/token", response_model=schemas.Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
+    user = auth.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=10)
+    access_token = auth.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    db_user = auth.get_user(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return auth.create_user(db=db, user=user)
+
+
+@router.get("/users/", dependencies=requires_auth, response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    users = auth.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@router.post("/users/items/", dependencies=requires_auth, response_model=schemas.Item)
+def create_item_for_user(
+    item: schemas.ItemCreate,
+    db: Session = Depends(database.get_db),
+    user: database.User = Depends(auth.get_current_active_user),
+):
+    return auth.create_user_item(db=db, item=item, user=user)
+
+
+@router.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    items = auth.get_items(db, skip=skip, limit=limit)
+    return items
