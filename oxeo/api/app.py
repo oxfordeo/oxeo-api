405650e42d320957d0b82a60bd41d10f8a69bbe0
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-06-08 08:53:39 +0000

    add magnum handler

--- oxeo/api/app.py
+++ oxeo/api/app.py
@@ -1,13 +1,9 @@
-import os
-
 from fastapi import FastAPI
+from mangum import Mangum
 
 from oxeo.api import routes
 
-SECRET = os.environ.get("SERVER-SECRET", "my-secret")
-
-# a quick create-all
-# database.Base.metadata.create_all(bind=database.engine)
-
 app = FastAPI()
 app.include_router(routes.router)
+
+handler = Mangum(app=app)
