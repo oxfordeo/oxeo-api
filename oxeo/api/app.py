d51892dcaf577f4af6fab03a6fd73573ac14ece0
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-06-08 14:19:12 +0000

    add a logging line

--- oxeo/api/app.py
+++ oxeo/api/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from loguru import logger
 from mangum import Mangum
 
 from oxeo.api import routes
@@ -10,6 +11,7 @@ app.include_router(routes.router)
 
 @app.exception_handler(404)
 async def custom_404_handler(_, __):
+    logger.info("REDIRECT")
     return RedirectResponse("/docs")
 
 
