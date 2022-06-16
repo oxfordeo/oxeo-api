94471a0de05b34e1e9a43da08ded72c446a3a6a7
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-06-16 17:44:26 +0000

    add dummy vars to pass CI

--- oxeo/api/controllers/authentication.py
+++ oxeo/api/controllers/authentication.py
@@ -20,13 +20,14 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
+# default to dummy data to pass CI
 mail_cfg = ConnectionConfig(
-    MAIL_USERNAME=os.environ.get("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD"),
-    MAIL_FROM=os.environ.get("MAIL_FROM_EMAIL"),
+    MAIL_USERNAME=os.environ.get("MAIL_USERNAME", "demo@oxfordeo.com"),
+    MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD", "demopass"),
+    MAIL_FROM=os.environ.get("MAIL_FROM_EMAIL", "demo@oxfordeo.com"),
     MAIL_PORT=587,
-    MAIL_SERVER=os.environ.get("MAIL_SERVER"),
-    MAIL_FROM_NAME=os.environ.get("MAIL_FROM_NAME"),
+    MAIL_SERVER=os.environ.get("MAIL_SERVER", "a_mail_server"),
+    MAIL_FROM_NAME=os.environ.get("MAIL_FROM_NAME", "demo@oxfordeo.com"),
     MAIL_TLS=True,
     MAIL_SSL=False,
     USE_CREDENTIALS=True,
