135ab5a926670bd3519f7bd98a5e91c2bb64476a
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-07-13 14:28:22 +0000

    extend password reset time

--- oxeo/api/routes.py
+++ oxeo/api/routes.py
@@ -27,7 +27,7 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=10)
+    access_token_expires = timedelta(minutes=30)
 
     access_token = C.auth.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
