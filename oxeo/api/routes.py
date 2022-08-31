f71305cb897806c5adb93a8a64cc9f1e6f37ef76
frandorr <fran.dorr@gmail.com>
2022-08-31 14:46:56 +0000

    Add models and forecast code

--- oxeo/api/routes.py
+++ oxeo/api/routes.py
@@ -3,6 +3,10 @@ import os
 from datetime import timedelta
 from typing import List, Optional, Union
 
+import gcsfs
+import pandas as pd
+import xarray as xr
+import xgboost as xgb
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -15,6 +19,26 @@ router = APIRouter()
 requires_auth = [Depends(C.auth.get_current_active_user)]
 admin_checker = C.auth.RoleChecker(["admin"])
 requires_admin = [Depends(C.auth.get_current_active_user), Depends(admin_checker)]
+
+
+def get_tp_cumsum(data, window=1):
+    cum_tp = (
+        data.mean(dim=["latitude", "longitude"]).isel(step=30).mean(dim="member").values
+    )
+    return (
+        pd.Series(cum_tp, index=data.time.values)
+        .rolling(min_periods=1, window=window)
+        .sum()
+    )
+
+
+def get_keyed_values(results, keyed_value, new_col):
+    df = pd.DataFrame(results)
+    df.labels = df.labels.map(lambda x: x[0])
+    df[new_col] = df.keyed_values.apply(lambda x: x.get(keyed_value))
+    df = df.drop_duplicates(subset=["aoi_id", "datetime"]).dropna()
+    df.datetime = pd.to_datetime(df.datetime)
+    return df
 
 
 def err_msg(key, val, type_ob):
@@ -109,6 +133,7 @@ def create_user(
 
     # else check token
     VALID_TOKENS = json.loads(os.environ.get("USER_TOKENS"))
+    print(type(user.token), type(VALID_TOKENS[0]), user.token in VALID_TOKENS)
     if user.token in VALID_TOKENS:
 
         db_user = C.auth.get_user(db, email=user.email)
@@ -236,6 +261,88 @@ def get_events(
 ):
 
     return C.geom.get_events(event_query=event_query, db=db, user=user)
+
+
+@router.get(
+    "/forecast/",
+    dependencies=requires_auth,
+    response_model=schemas.ForecastQueryReturn,
+    tags=["Forecast"],
+)
+def get_forecast(
+    db: Session = Depends(database.get_db),
+    user: database.User = Depends(C.auth.get_current_active_user),
+    event_query: schemas.EventQuery = Depends(bridges.to_eventquery),
+    forecast_query: schemas.ForecastQuery = Depends(bridges.to_forecastquery),
+):
+
+    bbox = forecast_query
+    events = C.geom.get_events(event_query=event_query, db=db, user=user)
+    events = [
+        {
+            "labels": e.labels,
+            "aoi_id": e.aoi_id,
+            "datetime": e.datetime,
+            "keyed_values": e.keyed_values,
+            "id": e.id,
+        }
+        for e in events.events
+    ]
+    aga_df = get_keyed_values(events, "mean_value", "ndvi_mean")
+    ndvi = aga_df.groupby(["datetime"]).mean()["ndvi_mean"]
+    ndvi_monthly = ndvi.resample("M").mean()
+
+    # Shift ndvi. I'm using last month ndvi mean, and tp_monthly forecast
+
+    url = "gs://oxeo-seasonal/tp"
+    zx = xr.open_zarr(gcsfs.GCSMap(url))
+
+    print(bbox)
+    min_x, min_y, max_x, max_y = bbox.bbox
+    min_x += 180
+    max_x += 180
+
+    data = zx["tp"].sel(
+        {
+            "time": slice(
+                ndvi_monthly.index.min(),
+                ndvi_monthly.index.max(),
+            ),
+            "latitude": slice(round(max_y), round(min_y)),
+            "longitude": slice(round(min_x), round(max_x)),
+        }
+    )
+
+    tp_monthly = get_tp_cumsum(data, window=1)
+    tp_monthly_2_cumsum = get_tp_cumsum(data, window=2)
+    tp_monthly_3_cumsum = get_tp_cumsum(data, window=3)
+
+    print(ndvi_monthly.shape, tp_monthly.shape)
+    ndvi_monthly = ndvi_monthly[:-1]
+
+    ndvi_monthly.index = tp_monthly.index
+
+    tp_monthly = tp_monthly[2:]
+    tp_monthly_2_cumsum = tp_monthly_2_cumsum[2:]
+    tp_monthly_3_cumsum = tp_monthly_3_cumsum[2:]
+    ndvi_monthly = ndvi_monthly[2:]
+
+    df = pd.concat(
+        [ndvi_monthly, tp_monthly, tp_monthly_2_cumsum, tp_monthly_3_cumsum], axis=1
+    )
+
+    # load XGB models
+    models = []
+    for i in range(7):
+        model = xgb.XGBRegressor()
+        model.load_model(f"{os.environ['MODELS_PATH']}/model_{i+1}.json")
+        models.append(model)
+
+    preds = []
+    for i in range(7):
+        preds.append(models[i].predict(df.values[-1].reshape(1, -1))[0])
+
+    return schemas.ForecastQueryReturn(forecast=list(preds))
 
 
 @router.post("/assets/", dependencies=requires_admin, status_code=200, tags=["Assets"])
